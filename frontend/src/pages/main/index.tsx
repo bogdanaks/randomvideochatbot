2c9c2bdf40e3a528c94333573eaabb3d4c708a9a
Bogdan <b.aksenov@yadro.com>
2023-06-22 01:04:43 +0300

    up

--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useMemo } from "react"
+import SimplePeer from "simple-peer"
+
 import { Wrapper } from "shared/ui/wrapper"
 
 import styles from "./styles.module.css"
 
+const simplePeer = new SimplePeer()
+
 const MainPage = () => {
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+        video: { facingMode: "user" },
+      })
+      .then(function (stream) {
+        console.log("steam", stream)
+      })
+  }, [])
+
   return (
     <Wrapper>
-      <div className={styles.container}>Hello World</div>
+      <div className={styles.container}>
+        Hello World
+        <span>
+          {SimplePeer.WEBRTC_SUPPORT
+            ? "WebRTC is supported in this browser"
+            : "WebRTC is not supported in this browser"}
+        </span>
+      </div>
     </Wrapper>
   )
 }
