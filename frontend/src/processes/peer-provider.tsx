5c9c100d797b859bd253a7e2945794f895637098
Bogdan <b.aksenov@yadro.com>
2023-06-28 00:28:59 +0300

    fix

--- frontend/src/processes/peer-provider.tsx
+++ frontend/src/processes/peer-provider.tsx
@@ -30,8 +30,9 @@ export const PeerProvider = () => {
 
   const peer = new Peer(String(user.id), {
     host: config.PEER_HOST,
+    port: config.PEER_PORT,
+    path: config.PEER_PATH,
     secure: true,
-    path: "/",
   })
 
   useEffect(() => {
